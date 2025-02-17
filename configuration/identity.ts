37124c59e645922481e9fe9a05d0656fbe1ac353
osamasalahm <Osama_Abdelrahman@kenan-flagler.unc.edu>
2025-02-17 12:17:56 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `Nakul`;
+export const OWNER_NAME: string = `CEM Contracting`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `[AI NAME]`;
+export const AI_NAME: string = `Sam`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
